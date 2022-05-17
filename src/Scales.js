68470232d2e5de4ae13069f9f3d0cfcd05df131e
zhanknight <zhanknight@gmail.com>
2022-05-16 21:01:03 -0400

    Add more scales data

--- src/Scales.js
+++ src/Scales.js
@@ -12,12 +12,101 @@ export const scaleData = [
     },
     {
         "id": "2",
+        "scale": "C# Major",
+        "root": "C#",
+        "type": "Major",
+        "notes": [
+            "C#", "D#", "E#", "F#", "G#", "A#", "B#"
+        ]
+    },
+    {
+        "id": "3",
         "scale": "D Major",
         "root": "D",
+        "type": "Major",
+        "notes": [
+            "D", "E", "F#", "G", "A", "B", "C#"
+        ]
+    },
+    {
+        "id": "4",
+        "scale": "Db Major",
+        "root": "Db",
+        "type": "Major",
+        "notes": [
+            "Db", "Eb", "F", "Gb", "Ab", "Bb", "C"
+        ]
+    },
+    {
+        "id": "5",
+        "scale": "E Major",
+        "root": "E",
         "type": "Major",
         "notes": [
             "C", "D", "E", "F", "G", "A", "B"
         ]
     },
-    
+    {
+        "id": "6",
+        "scale": "F Major",
+        "root": "F",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
+    {
+        "id": "7",
+        "scale": "F# Major",
+        "root": "F#",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
+    {
+        "id": "8",
+        "scale": "G Major",
+        "root": "G",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
+    {
+        "id": "9",
+        "scale": "G# Major",
+        "root": "G#",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
+    {
+        "id": "10",
+        "scale": "A Major",
+        "root": "A",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
+    {
+        "id": "11",
+        "scale": "A# Major",
+        "root": "A#",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
+    {
+        "id": "12",
+        "scale": "B Major",
+        "root": "B",
+        "type": "Major",
+        "notes": [
+            "C", "D", "E", "F", "G", "A", "B"
+        ]
+    },
                     ];

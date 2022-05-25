857c4e4f9ca06d6a49dddcae62e1a384608b5a2a
zhanknight <zhanknight@gmail.com>
2022-05-24 23:12:17 -0400

    Small style adjustments

--- src/Resultbox.js
+++ src/Resultbox.js
@@ -3,16 +3,21 @@ function Resultbox(props) {
 
     return (
         // just show me everything in the object right now
-        <div class="App-item"> <h3> RESULT: </h3><br />
+        <div class="App-item"> 
        <h4>Scale:</h4>{props.data.scale}<br />
-       <h4>Notes in the scale:</h4> {props.data.notes}<br />
+       <h4>Notes in the scale:</h4><br />
+                <ul>
+                {props.data.notes.map(data => {
+                    return (
+                        <li key={data}>{data}</li>
+                    )
+                }) }
+                </ul>
        <h4>Root of the scale:</h4> {props.data.root}<br />
        <h4>Scale Quality:</h4> {props.data.type}<br />
         </div>
     )
 }
-
-//        {props.data.display.toString()} 
 
 export default Resultbox;
 

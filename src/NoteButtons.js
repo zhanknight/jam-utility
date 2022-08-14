3d4582226cec5ab6c346d79787cd72962a1f7fa2
zhanknight <zhanknight@gmail.com>
2022-08-14 17:46:12 -0400

    Change note buttons to piano style layout closes #4

--- src/NoteButtons.js
+++ src/NoteButtons.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 function NoteButtons(props) {
-    const notenames = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"]
+    const naturals = ["C", "D", "E", "F", "G", "A", "B"]
+    const accidentals = ["C#/Db","D#/Eb","F#/Gb","G#/Ab", "A#/Bb"]
     const [interimNotes, setInterimNotes] = useState([])
 
 // this function sends a selected note back to the parent component for filtering
@@ -47,17 +48,31 @@ function NoteButtons(props) {
 // map out a button for each note that calls noteToggle when clicked
     return (
         <div className="App-selectorbar"> <h4>Select notes to see scales that contain those notes.</h4>
-        <div data-testid="buttons">
-        {notenames.map(note => {
+        <div data-testid="buttons" className="keyboard-container">
+        <div className="accidentals-container">
+        {accidentals.map(note => {
             if (interimNotes.includes(note)) {
             return (
-                <button className="button-selected" key={note} onClick={() => {noteToggle(note)}}>{note}</button>
+                <button className={note[0]} id="button-selected" key={note} onClick={() => {noteToggle(note)}}>{note}</button>
             )
             }
             else return (
-                <button key={note} onClick={() => {noteToggle(note)}}>{note}</button>
+                <button className={note[0]} key={note} onClick={() => {noteToggle(note)}}>{note}</button>
             )
         }) }
+        </div>
+        <div className="naturals-container">
+        {naturals.map(note => {
+            if (interimNotes.includes(note)) {
+            return (
+                <button className={note[0]} id="button-selected" key={note} onClick={() => {noteToggle(note)}}>{note}</button>
+            )
+            }
+            else return (
+                <button className={note[0]} key={note} onClick={() => {noteToggle(note)}}>{note}</button>
+            )
+        }) }
+        </div>
         <button className="button-reset" onClick={() => {resetButton()}}>Reset</button>
       </div>
       </div>

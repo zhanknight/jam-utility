91cbb6a817178f75fefbd7769c0f8a13d04a10b6
zhanknight <20487008+zhanknight@users.noreply.github.com>
2024-01-24 10:44:14 -0500

    Initialize JamUtility v2 as Blazor WASM project

--- src/NoteButtons.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import NoteButtons from './NoteButtons';
-
-test('Renders button div', () => {
-  render(<NoteButtons />);
-  const buttonElement = screen.getByTestId("buttons");
-  expect(buttonElement).toBeInTheDocument();
-});

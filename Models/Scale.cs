4f4f0040dfd81245945c3b070c66eff62727d86f
zhanknight <20487008+zhanknight@users.noreply.github.com>
2024-02-02 14:23:14 -0500

    Initial scalebuilder

--- Models/Scale.cs
+++ Models/Scale.cs
@@ -5,6 +5,7 @@ public class Scale
     public int id { get; set; }
     public string Root { get; set; }
     public string Type { get; set; }
-    public List<Note> Notes { get; set; }
+    public List<string> Notes { get; set; }
+    public List<string> Chords { get; set; }
     
 }
---
+++ Models/Scale2.cs
@@ -0,0 +1,10 @@
+﻿namespace JamUtility.Models;
+
+public class Scale2
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public List<int> Definition { get; set; }
+    public List<string> Chords { get; set; }
+    
+}
